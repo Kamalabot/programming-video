dd593627c542b3e162a662bf508511e9f96d8487
kamalabot <kamaljp@gmail.com>
2025-07-20 15:33:58 +0530

    finished exercise

--- challenges/01_walking_in_circles/walking_in_circles.gd
+++ challenges/01_walking_in_circles/walking_in_circles.gd
@@ -2,4 +2,24 @@ extends SteppingRobot
 
 
 func what_should_i_do():
-	pass
+	#turn_left()
+	#step_forward()
+	#turn_left()
+	#step_forward()
+	#turn_left()
+	#turn_left()
+	#for o in 4:
+		#for d in 4:
+			#step_forward()
+		#turn_right()
+	var cups = 0
+	if is_item_before_me == true:
+		turn_right()
+		step_forward()
+		turn_left()
+	while cups < 3:
+		while is_item_before_me == true:
+			cups += 1
+			turn_right()
+			step_forward()
+		step_forward()
--- welcome.gd
+++ welcome.gd
@@ -1,5 +1,37 @@
 extends SteppingRobot
 
+var price = 5
+
 func what_should_i_do():
-	step_forward()
+	#for d in 3:
+		#step_forward()
+	#pick_up_item()
+	#turn_around()
+	#for d in 3:
+		#step_forward()
+		#say("I am moving...")
+		
+	#if is_item_before_me == true:
+		#say("There is an Item")
+	#turn_around()
+	#drop_item()
+	var cal = 5 * 5
+	var price_for_coffee = 1
+	var coins = 100
+	var avbl_coins = coins - (price_for_coffee * cal)
+	#var extra = "Super I have " + str(avbl_coins)
+	#say(extra)
+	while is_item_before_me == false:
+		step_forward()
+	say("There is an item now")
+func calc_and_say(amount_coffee: int) -> int:
+	var result = price * amount_coffee
+	var discount = 1
+	if amount_coffee >= 3:
+		say("Awesome " + str(discount) + " coin discount recieved...")
+		result -= 1
+	return result
+		
+func  turn_around():
+	turn_left()
 	turn_left()
